c6316ea04366e288f25f30a987bc2ce162b71955
Paulo <Paulo_Pereira@epam.com>
2020-07-26 12:54:05 +0200

    Game completion

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 const config = {
     width: 5,
     height: 5,
-    loopingDelay: 100
+    loopingDelay: 100,
+    colors: ['red', 'gray', 'green']
 }
 
 class Tick {
@@ -21,8 +22,20 @@ class Tick {
 
 class Game {
     game = null;
+    scoreDom = null;
+    lifeDom = null;
+    chancesDom = null;
+    levelDom = null;
     cells = [];
+    rows = [];
     currentTime = 0;
+    score = 0;
+    chances = 10;
+    life = 3;
+    level = 1;
+    height = config.height;
+    width = config.width;
+    gameOver = false;
 
     static time = null;
 
@@ -35,7 +48,6 @@ class Game {
             this.instatiate();
         } else {
             document.addEventListener("DOMContentLoaded", () => {
-                console.log("Test");
                 this.instatiate();
             })
         }
@@ -44,45 +56,157 @@ class Game {
     }
 
     instatiate() {
+        Game.time = new Tick();
         this.game = document.getElementById("game");
+        this.levelDom = document.getElementById("level");
+        this.lifeDom = document.getElementById("life");
+        this.chancesDom = document.getElementById("chances");
+        this.scoreDom = document.getElementById("score");
 
-        for(let i=0; i<config.height; i++) {
-            const row = document.createElement("div");
-            this.game.appendChild(row);
-            for(let j=0; j<config.width; j++) {
-                this.cells.push(new Cell(row));
+        this.levelDom.innerText = this.level;
+        this.lifeDom.innerText = this.life;
+        this.chancesDom.innerText = this.chances;
+        this.scoreDom.innerText = this.score;
+        this.instatiateBoard();
+    }
+
+    instatiateBoard() {
+        for(let i=0; i<this.height; i++) {
+            const row = i <= this.rows.length - 1 ? this.rows[i] : this.addRow();
+            for(let j=0; j<this.width; j++) {
+                if(j > row.childNodes.length - 1) {
+                    this.addCell(row);
+                }
             }
         }
+    }
 
-        Game.time = new Tick();
+    addRow() {
+        const row = document.createElement("div");
+        this.game.appendChild(row);
+        this.rows.push(row);
+
+        return row;
+    }
+
+    addCell(row) {
+        this.cells.push(new Cell(row, this));
     }
 
     update() {
-        this.currentTime += Game.time.deltaTime;
+        if(!this.gameOver) {
+            this.cells.forEach(c => c.update());
+        }
+    }
 
-        this.cells.forEach(c => c.update());
+    addScore() {
+        this.score += 1;
+        this.scoreDom.innerText = this.score;
+
+        if(this.score > (10 * this.level * this.level)) {
+            this.increaseLevel();
+        }
+    }
+
+    removeLife() {
+        this.life -= 1;
+        this.lifeDom.innerText = this.life;        
+
+        if(this.life == 0) {
+            this.setGameOver();
+        }
+    }
+
+    removeChance() {
+        this.chances -= 1;
+        this.chancesDom.innerText = this.chances;
+
+        if(this.chances == 0) {
+            this.setGameOver();
+        }
+    }
+
+    setGameOver() {
+        this.gameOver = true;
+        const savedRecord = localStorage.getItem("record");
+        const record = (savedRecord === 'null' || this.score > savedRecord ? this.score : savedRecord) || this.score;
+        localStorage.setItem("record", record);
+        this.game.innerHTML = `<h1>Game Over</h1><h2>Score:${this.score}<h2><h3>Record:${record}<h3>`;
+    }
+
+    increaseLevel() {
+        this.level += 1;
+        this.height += 1;
+        this.width += 1;
+
+        this.levelDom.innerText = this.level;
+
+        this.cells.forEach(cell => cell.changeColor('white'));
+
+        this.instatiateBoard();
     }
 }
 
 class Cell {
     parentNode = null;
+    color = null;
+    minTimeToChange = 3;
+    maxTimeToChange = 15;
+    timeTochange = 0;
+    currentTime = 0;
 
-    constructor (parentNode) {
+    constructor (parentNode, game) {
         this.parentNode = parentNode;
+        this.game = game;
         this.dom = document.createElement("span");
         this.dom.className = 'cell';
         this.parentNode.appendChild(this.dom);
-
         this.dom.addEventListener("click", this.click.bind(this));
+        this.getNewTime();
     }
 
     click() {
-        console.log(Game.time.deltaTime);
+        if(this.color === 'green') {
+            this.currentTime = 0;
+            this.changeColor('gray', true);
+            this.game.addScore();
+        } else if(this.color === 'red') {
+            this.currentTime = 0;
+            this.game.removeLife();
+            this.changeColor('black', true);
+        }
+
+    }
+
+    getNewTime () {
+        this.timeTochange = randomRange(this.minTimeToChange, this.maxTimeToChange);
+    }
+
+    changeColor(color, playerClick) { 
+        if(this.color === 'green' && color !== 'white' && !playerClick) {
+            this.game.removeChance();
+            color = 'black';
+        }
+
+        this.color = color || config.colors[randomRange(0, config.colors.length)];
+        this.dom.className = 'cell ' + this.color;
     }
 
     update() {
+        if(Game.time) {
+            this.currentTime += Game.time.deltaTime;
 
+            if(this.currentTime > this.timeTochange) {
+                this.currentTime = 0;
+                this.changeColor();
+                this.getNewTime();
+            }
+        }
     }
+}
+
+function randomRange(min, max) {
+    return min + Math.floor(Math.random() * max);
 }
 
 new Game();
